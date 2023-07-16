3b30d9a72f5222a2fb20ec2024cddd1ad145f389
Jasper <jasper8777@icloud.com>
2023-07-16 10:16:53 -0700

    added row-adding functionality on the form, require more modification

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,13 @@
-const pasteBtn1 = document.querySelector(".paste1");
-const pasteBtn2 = document.querySelector(".paste2");
-const pasteArea1 = document.querySelector(".singleProduct-halfGrid-image--1");
-const pasteArea2 = document.querySelector(".singleProduct-halfGrid-image--2");
+const pasteBtns = document.querySelectorAll(".pasteBtn");
 const userInputBtn = document.querySelector(".enter");
 const userInputForm = document.getElementById("userInputForm");
-const extraSizeBtns = document.querySelectorAll('.extraSizeBtn')
+const extraSizeBtns = document.querySelectorAll(".extraSizeBtn");
 
 let userInputData;
 
-navigator.clipboard
-  .readText()
-  .then((clipText) => {
-    console.log('clipboard text:', clipText);
-  });
+navigator.clipboard.readText().then((clipText) => {
+  console.log("clipboard text:", clipText);
+});
 
 async function parseClipboardData(pasteTarget) {
   const items = await navigator.clipboard.read().catch((err) => {
@@ -24,7 +19,7 @@ async function parseClipboardData(pasteTarget) {
         item.getType(type).then((imageBlob) => {
           const image = `<img src="${window.URL.createObjectURL(
             imageBlob
-          ) }" />`;
+          )}" />`;
           let existingImage = pasteTarget.children;
           if (existingImage) {
             existingImage.remove;
@@ -37,39 +32,45 @@ async function parseClipboardData(pasteTarget) {
   }
 }
 
-
-
-pasteBtn1.addEventListener("click", () => {
-  console.log("click me");
-
-  parseClipboardData(pasteArea1);
+pasteBtns.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    console.log(typeof e.target.dataset.target);
+    const pasteTarget = document.getElementById(e.target.dataset.target);
+    parseClipboardData(pasteTarget);
+  });
 });
 
-pasteBtn2.addEventListener("click", () => {
-  console.log("click me");
+// pasteBtn1.addEventListener("click", () => {
+//   // console.log("click me");
 
-  parseClipboardData(pasteArea2);
-});
+//   parseClipboardData(pasteArea1);
+// });
+
+// pasteBtn2.addEventListener("click", () => {
+//   // console.log("click me");
+
+//   parseClipboardData(pasteArea2);
+// });
 
 const mapFromDataToSheet = (userInputForm) => {
   let userInputData = new FormData(userInputForm);
   for (const pair of userInputData.entries()) {
     // excludes barcode from being mapped
-    if (!pair[1]) { continue; }
-    if (pair[0].includes('barcode')) { 
+    if (!pair[1]) {
+      continue;
+    }
+    if (pair[0].includes("barcode")) {
       JsBarcode(`#${pair[0]}`, pair[1]);
-
-     }
-    document.getElementById(`${ pair[0] }`).innerText = '';
-    document.getElementById(`${ pair[0] }`).innerText = `${ pair[1] }`;
+    }
+    document.getElementById(`${pair[0]}`).innerText = "";
+    document.getElementById(`${pair[0]}`).innerText = `${pair[1]}`;
     console.log(`${pair[0]}, ${pair[1]}`);
   }
-}
+};
 
 userInputBtn.addEventListener("click", () => {
   mapFromDataToSheet(userInputForm);
-  
-})
+});
 
 // let formState = {
 //   form1: {
@@ -80,11 +81,12 @@ userInputBtn.addEventListener("click", () => {
 //   }
 // };
 
-
 const displayExtraSizeInput = (btn) => {
   // formState.form1.numOfSizes = 1++;
 
-  btn.insertAdjacentHTML("beforeBegin", `
+  btn.insertAdjacentHTML(
+    "beforeBegin",
+    `
   <label for="size4">4th Size Name:</label>
   <input type="text" name="size4">
   <label for="barcode4">4th UPC Barcode</label>
@@ -92,17 +94,19 @@ const displayExtraSizeInput = (btn) => {
   <label for="sku4">4th SKU Number</label>
   <input type="text" name="sku4">
   <br>
-  `);
-  
-}
+  `
+  );
+};
 
-extraSizeBtns.forEach(btn => {
-  btn.addEventListener('click', (e) => {
-    console.log('extra size generate!');
+extraSizeBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log("extra size generate!");
     console.log(e.target);
     console.log(e.target.id);
 
     displayExtraSizeInput(e.target);
-  })
-})
+  });
+});
 
+// refactor suggestion:
+// use builder pattern for inputs
